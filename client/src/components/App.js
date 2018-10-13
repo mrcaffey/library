5708d68372e584c136342747b35a576d1dcfce72
Micah Caffey <mrcaffey@gmail.com>
2018-10-12 21:37:48 -0600

    changes to App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,33 +1,24 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
 import NoMatch from './NoMatch';
 import NavBar from './NavBar';
-import Login from './Login';
-import Register from './Register';
 import Flash from './Flash';
 import Home from './Home';
-import ProtectedRoute from './ProtectedRoute';
-import AuthRoute from './AuthRoute';
-import FetchUser from './FetchUser';
+import { Switch, Route } from 'react-router-dom';
+import { Segment } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Segment basic>
         <NavBar />
         <Flash />
-        <FetchUser>
           <Switch>
-            <Route exact path="/" component={Home} />
-            <AuthRoute exact path="/login" component={Login} />
-            <AuthRoute exact path="/register" component={Register} />
+            <Route exact path='/' component={Home} />
             <Route component={NoMatch} />
           </Switch>
-        </FetchUser>
-      </div>
+      </Segment>
     );
   }
 }
 
 export default App;
-
